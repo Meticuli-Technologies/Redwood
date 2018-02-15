7f34da4ccc44f1d3b0ab4f9b688089d890ae90f0
Mathhman <minecraftmathhman@gmail.com>
2018-02-15 16:33:18 -0500

    Cleaning code up, made a lesson class for compaction

---
+++ src/com/meti/Lesson.java
@@ -0,0 +1,15 @@
+package com.meti;
+
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 2/15/2018
+ */
+public class Lesson {
+    public Lesson(List<String> pres, List<String> subs, List<String> pages) {
+
+    }
+}
--- src/com/meti/TreeView.java
+++ src/com/meti/TreeView.java
@@ -4,10 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -43,6 +40,7 @@ public class TreeView implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         setUpContent();
 
+        List<Lesson> lessons = new ArrayList<>();
         Path path = Paths.get(".\\lessons");
         try {
             List<Path> mainPaths = Files.
@@ -50,14 +48,22 @@ public class TreeView implements Initializable {
                     filter(path1 -> path1.toString().contains("main")).
                     collect(Collectors.toList());
             for (Path mainPath : mainPaths) {
-                loadMain(mainPath);
+                lessons.add(loadMain(mainPath));
             }
         } catch (IOException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
         }
+
+        for (Lesson lesson : lessons) {
+            TitledPane titledPane = new TitledPane();
+            titledPane.setTranslateX(width / 2);
+            titledPane.setTranslateY(height / 2);
+            titledPane.setPrefSize(100, 100);
+            content.getChildren().add(titledPane);
+        }
     }
 
-    private void setUpContent(){
+    private void setUpContent() {
         width = 1000;
         height = 1000;
         content.setMinSize(width, height);
@@ -65,18 +71,53 @@ public class TreeView implements Initializable {
         scrollPane.setVvalue(scrollPane.getVmin());
     }
 
-    private void loadMain(Path mainPath) throws IOException, ParserConfigurationException, SAXException {
+    private Lesson loadMain(Path mainPath) throws IOException, ParserConfigurationException, SAXException {
         BufferedReader reader = Files.newBufferedReader(mainPath);
+
+        List<String> pres = new ArrayList<>();
+        List<String> subs = new ArrayList<>();
         List<String> pages = new ArrayList<>();
 
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
         Document document = builder.parse(Files.newInputStream(mainPath));
         document.getDocumentElement().normalize();
-        NodeList nodeList = document.getElementsByTagName("page");
 
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node = nodeList.item(i);
+        NodeList preList = document.getElementsByTagName("preList");
+        for (int i = 0; i < preList.getLength(); i++) {
+            Node node = preList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String content = element.getTextContent();
+                List<String> contentList = new ArrayList<>();
+                Arrays.stream(content.split(" "))
+                        .filter(s -> !s.trim().equals(""))
+                        .collect(Collectors.toList())
+                        .forEach(s -> contentList.add(s.trim().replace("\n", "")));
+                pres.addAll(contentList);
+            }
+        }
+
+        NodeList subList = document.getElementsByTagName("subList");
+        for (int i = 0; i < subList.getLength(); i++) {
+            Node node = subList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String content = element.getTextContent();
+                List<String> contentList = new ArrayList<>();
+                Arrays.stream(content.split(" "))
+                        .filter(s -> !s.trim().equals(""))
+                        .collect(Collectors.toList())
+                        .forEach(s -> contentList.add(s.trim().replace("\n", "")));
+                subs.addAll(contentList);
+            }
+        }
+
+        NodeList pageList = document.getElementsByTagName("page");
+        for (int i = 0; i < pageList.getLength(); i++) {
+            Node node = pageList.item(i);
 
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
@@ -92,10 +133,6 @@ public class TreeView implements Initializable {
 
         reader.close();
 
-        TitledPane titledPane = new TitledPane();
-        titledPane.setTranslateX(width / 2);
-        titledPane.setTranslateY(height / 2);
-        titledPane.setPrefSize(100, 100);
-        content.getChildren().add(titledPane);
+        return new Lesson(pres, subs, pages);
     }
 }
